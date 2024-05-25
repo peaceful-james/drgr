d4c4a17a6e7985c46d3d49f16531ab93fcb922e2
James Gough <james@peacefulprogramming.dev>
2024-05-25 15:49:08 +0100

    Show the problem happens even if code only called once

--- lib/drgr_web/live/demo_live.ex
+++ lib/drgr_web/live/demo_live.ex
@@ -2,10 +2,17 @@ defmodule DrgrWeb.DemoLive do
   @moduledoc false
   use DrgrWeb, :live_view
 
+  def mount(_params, _session, socket) do
+    socket
+    |> assign(:connected?, connected?(socket))
+    |> then(&{:ok, &1})
+  end
+
   @impl true
   def render(%{} = assigns) do
     ~H"""
     <div
+      :if={@connected?}
       id="demo-dragdoll"
       phx-hook="DragdollHook"
       class={[
